f86679056d209e3348c2fe2246f75bcdec1e0ae9
Daniel <daniel.revie1@gmail.com>
2018-03-11 13:50:56 -0400

    enabling fa icons

--- src/index.tsx
+++ src/index.tsx
@@ -2,6 +2,7 @@ import * as React from 'react';
 import * as ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import registerServiceWorker from './registerServiceWorker';
+import 'font-awesome/css/font-awesome.min.css';
 import './scss/index.scss';
 import './scss/App.scss';
 import App from './components/App';
