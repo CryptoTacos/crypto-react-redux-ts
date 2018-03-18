7dfc9e92aa17dd412ddb01180111616e3a240fa6
Daniel <daniel.revie1@gmail.com>
2018-03-18 12:34:18 -0400

    creating new components and tabbed view for coin detail card

--- src/containers/CoinDashboardContainer.tsx
+++ src/containers/CoinDashboardContainer.tsx
@@ -4,6 +4,8 @@ import * as actions from '../actions/coinDashboardActions';
 import { StoreState, FlattenedCoinData } from '../types';
 import CoinCardDetail from '../components/CoinCardDetail';
 import CryptoCard from '../components/CryptoCard';
+import { Tabs, Tab } from 'material-ui/Tabs';
+const cryptocurrencies = require('cryptocurrencies');
 
 interface CoinDashboardProps {
     coinData: FlattenedCoinData[];
@@ -23,16 +25,37 @@ class CoinDashboardContainer extends React.Component<CoinDashboardProps, CoinDas
         const coinCards: JSX.Element[] = [];
         for (const coin of this.props.coinData) {
             coinCards.push(
-                <CryptoCard
-                    title={coin.name}
-                    avatarPath={require(`../icons/coins/color/${coin.USD.FROMSYMBOL.toLowerCase()}.svg`)}
-                >
-                    <CoinCardDetail
-                        ticker={coin.USD.TOSYMBOL}
-                        coinData={coin}
-                        currencyContext={'USD'}
-                    />
-                </CryptoCard>
+                <div className="coin-card-flex-item">
+                    <CryptoCard
+                        title={cryptocurrencies[coin.name]}
+                        subtitle={coin.name}
+                        avatarPath={require(`../icons/coins/color/${coin.USD.FROMSYMBOL.toLowerCase()}.svg`)}
+
+                    >
+                        <Tabs>
+                            <Tab label="Market Data">
+                                <CoinCardDetail
+                                    ticker={coin.USD.TOSYMBOL}
+                                    coinData={coin}
+                                    currencyContext={'USD'}
+                                />
+                            </Tab>
+                            <Tab label="Twitter Feed">
+                                <p>
+                                    Here we can have a twitter feed of the latest coin relevant tweets,
+                                    or any feed about the coin for that matter
+                                </p>
+                            </Tab>
+                            <Tab label="Coin Bio">
+                                <p>
+                                    Potentially whitepaper here
+                                </p>
+                            </Tab>
+
+                        </Tabs>
+                    </CryptoCard>
+
+                </div>
             );
         }
         return coinCards;
@@ -40,7 +63,7 @@ class CoinDashboardContainer extends React.Component<CoinDashboardProps, CoinDas
 
     render() {
         return (
-            <div>
+            <div className="coin-card-dashboard">
                 {this.getCoinCards()}
             </div>
         );
