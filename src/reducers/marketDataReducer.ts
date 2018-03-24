3b15005ebf88df6fa16eacd99d7d4a54a0faa21e
Daniel <daniel.revie1@gmail.com>
2018-03-23 21:05:24 -0400

    dashboard container is now mapped to the current and historical data

--- src/reducers/marketDataReducer.ts
+++ src/reducers/marketDataReducer.ts
@@ -1,20 +1,26 @@
 
 import { MarketDataState } from '../types/index';
-import { MarketDataActions } from '../actions/marketDataActions';
-import { SET_HISTORICAL_MARKET_DATA } from '../constants';
+import { MarketDataActions } from '../actions/historicalMarketDataActions';
+import { CurrentMarketDataAction } from '../actions/currentMarketDataActions';
+import { SET_HISTORICAL_MARKET_DATA, SET_CURRENT_MARKET_DATA } from '../constants';
 
 const initialState: MarketDataState = {
-    coinData: [],
+    currentMarketData: [],
     historicalMarketData: [],
 };
 
-const marketDataReducer = (state = initialState, action: MarketDataActions):
-    MarketDataState => {
+// tslint:disable-next-line:max-line-length
+const marketDataReducer = (state = initialState, action: MarketDataActions | CurrentMarketDataAction): MarketDataState => {
     switch (action.type) {
         case SET_HISTORICAL_MARKET_DATA:
             return {
                 ...state,
                 historicalMarketData: action.historicalMarketData,
+            };
+        case SET_CURRENT_MARKET_DATA:
+            return {
+                ...state,
+                currentMarketData: action.currentMarketData
             };
         default:
             return state;
