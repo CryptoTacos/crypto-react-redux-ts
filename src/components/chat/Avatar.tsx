be8ed907e0bba643d954856b793a6d9200da13fa
Daniel J Revie <daniel.revie1@gmail.com>
2018-05-05 12:45:09 -0400

    Feature/chat bot (#20)
    * initial commit for continuous candlestick chart
    
    * adding api call for historical market data
    
    * api call succesfully fetching market data, need to improve the asynchronicity as the calls are taking a while synchronously
    
    * charts work now
    
    * dispatch after each api call
    
    * adding continuous chart
    
    * adding api calls for hourly daily and minutely data
    
    * converting timestamp from milliseconds to seconds before creating new date object
    
    * removing old  grid
    
    * action files are now cleaner
    
    * router homepage is now the card views
    
    * dashboard container is now mapped to the current and historical data
    
    * modifying store state reducer and types because old grid reducer was removed
    
    * implementing async api calls with redux-observables
    
    * creating mock api call for users's pinned coins
    
    * adding searchable coins and material ui search component
    
    * allow any types to make this app fun again
    
    * cleaning up types directory
    
    * framing out chat bot

---
+++ src/components/chat/Avatar.tsx
@@ -0,0 +1,24 @@
+import * as React from 'react';
+
+interface IAvatarProps {
+
+}
+
+interface IAvatarState {
+
+}
+
+class Avatar extends React.Component<IAvatarProps, IAvatarState> {
+    constructor(props: IAvatarProps) {
+        super(props);
+        this.state = {};
+    }
+
+    render() {
+        return (
+            <div/>
+        );
+    }
+}
+
+export default Avatar;
